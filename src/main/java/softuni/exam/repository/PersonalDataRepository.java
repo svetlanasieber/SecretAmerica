8c952daddea0293863b134a7eafac14291d82235
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-04-03 04:59:07 +0300

    Update PersonalDataRepository.java

--- src/main/java/softuni/exam/repository/PersonalDataRepository.java
+++ src/main/java/softuni/exam/repository/PersonalDataRepository.java
@@ -1,7 +1,5 @@
 package softuni.exam.repository;
 
-//ToDo:
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.models.entity.PersonalData;
